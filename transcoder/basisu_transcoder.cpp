118766bb53bd2485391d84e8c1a9d2e556201262
Johan Smet <johan.smet@justcode.be>
2025-07-18 23:18:35 +0200

    Fix compiler warning - unsigned int is always positive

--- transcoder/basisu_transcoder.cpp
+++ transcoder/basisu_transcoder.cpp
@@ -22343,7 +22343,7 @@ namespace basist
 						if (d >= mid_dots[mid]) low = mid + 1;
 
 						best_idx = low;
-						assert((best_idx >= 0) && (best_idx <= 15));
+						assert(best_idx <= 15);
 
 						pWeights[i] = (uint8_t)best_idx;
 
@@ -22375,7 +22375,7 @@ namespace basist
 						if (d >= mid_dots[mid]) low = mid + 1;
 
 						best_idx = low;
-						assert((best_idx >= 0) && (best_idx <= 15));
+						assert(best_idx <= 15);
 
 						float err = basisu::squaref(qr - cr[best_idx]) + basisu::squaref(qg - cg[best_idx]) + basisu::squaref(qb - cb[best_idx]);
 
